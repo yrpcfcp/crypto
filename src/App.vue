f3050033a53ff6056feacfcc667dce5f7bf7ae9d
yrpcfcp <carlosfparana@gmail.com>
2022-11-01 23:45:57 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,129 @@
+
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+<template>
+  <header>
+    <h1>PRICE GENERATOR</h1>
+  </header><br>
+  <main>
+    <ul>
+      <li>
+        ATOM<br><div>{{ atomPrice }}</div>
+      </li>
+      <li>
+        BITCOIN<br>
+        <div>{{ bitCoinPrice }}</div>
+      </li>
+      <li>
+        DAXCI COIN<br><div>{{ daxciCoinPrice }}</div>
+      </li>
+      <li>
+        ETHEREUM <br>{{ ethereumPrice }}
+      </li>
+      <li>
+        LUNA <br>{{ lunaPrice }}
+      </li>
+    </ul>
+  </main>
+  <article>
+    <label for="data">
+      <h1>ESCOLHA A DATA PARA VER O PREÇO HISTÓRICO</h1>
+    </label>
+    <br><br>
+    <input type="date" id="date" name="date">
+    <h4>preço em {{}}: (preço)</h4>
+    <button v-on:click="created()">get prices</button>
+
+  
+  </article>
+</template>
+
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+
+      atomPrice: 0,
+      bitCoinPrice: 0,
+      daxciCoinPrice: 0,
+      ethereumPrice: 0,
+      lunaPrice: 0
+      
+    }
+  }, methods: {
+    async created() {
+      try {
+        const res = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&amp;vs_currencies=usd&amp;include_market_cap=true&amp;include_24hr_vol=true&amp;include_last_updated_at=true');
+        console.log(res);
+        const bitCoinPrice = res.data.bitcoin.usd
+        this.bitCoinPrice = bitCoinPrice;
+
+
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+}
+
+
+</script>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,500;0,700;1,400&display=swap');
+
+body {
+  background-color: black;
+  color: aqua;
+  font-family: 'Rubik', sans-serif;
+}
+
+header {
+  color: black;
+  padding: 35px;
+}
+
+header h1 {
+  color: aqua;
+  text-align: center;
+  margin: 5px 0px 0px 0px;
+  font-size: 40px;
+}
+
+h2 {
+  color: aqua;
+  margin: 100px 0px 0px 0px;
+  text-align: center;
+}
+
+h3 {
+  color: aqua;
+  margin: 100px 0px 0px 0px;
+  text-align: center;
+}
+
+
+
+article {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 100px;
+}
+
+input {
+  width: 15%;
+}
+
+ul {
+  display: flex;
+  justify-content: space-evenly;
+  list-style-type: none;
+}
+</style>
